eba53415324cb815078e3c9f76ea72497008bf91
K. Liutskevich <k.liutskevich@gmail.com>
2018-11-03 21:10:14 +0300

    правка цен для корзины для iOS

--- local/templates/trals/components/bitrix/sale.basket.order.ajax/webcompany_basket_ankuse/script.js
+++ local/templates/trals/components/bitrix/sale.basket.order.ajax/webcompany_basket_ankuse/script.js
@@ -1,4 +1,6 @@
-(function (window) {
+'use strict';
+
+(function (window, $) {
     $(document).ready(function () {
 
         /* Без выбора чекбокса цену не показываем, а при выборе и смене чекбокса меняем цену за шт*/
@@ -7,32 +9,30 @@
 
         /* Функция принимает 2 аргумента, первый показывает, второй скрывает */
         function hideOneOfPrice(tab1, tab2) {
-            $(document).find(`span[id=${tab1}]`).css('display', 'block').attr('data-current-price', 'current-price');
-            $(document).find(`span[id=${tab2}`).css('display', 'none').attr('data-current-price', '');
+            $(document).find('span[id=' + tab1 + ']').css('display', 'block').attr('data-current-price', 'current-price');
+            $(document).find('span[id=' + tab2 + ']').css('display', 'none').attr('data-current-price', '');
         }
 
         $('.entity-type__radio').on('click', function () {
             /*показываем цену за шт и сумму у строки (товара)*/
             var tab = $($('.tab[data-id="' + $(this).data('id') + '"]').get(0)).data('id');
             var showPriceForOne = $('span[id="' + tab + '"]');
-            if( tab === 'tab1') {
-                showPriceForOne.each((i, e) => {
+            if (tab === 'tab1') {
+                showPriceForOne.each(function (i, e) {
                     hideOneOfPrice('tab1', 'tab2');
-                    sumOneItem(e)
-                })
+                    sumOneItem(e);
+                });
             } else {
-                showPriceForOne.each((i, e) => {
+                showPriceForOne.each(function (i, e) {
                     hideOneOfPrice('tab2', 'tab1');
-                    sumOneItem(e)
-                })
+                    sumOneItem(e);
+                });
             }
             setItogoPrice();
         });
 
-
-
         // функция принимает элемент и устанавливает цену для этого элемента.
-        function sumOneItem (e) {
+        function sumOneItem(e) {
             var priceforItem = Number($(e).data('price-oneitem'));
             var quantity = Number($(e).closest("tr").find('input').val());
             var sum = priceforItem * quantity;
@@ -49,23 +49,23 @@
                 pr = $(this).closest('tr').find(showPriceForOne).data('price-oneitem'),
                 sumValPer = val * pr;
             $(this).closest('tr').find('.sumOneItem').text(sumValPer.toFixed(2));
-            var weightFixed =  val*weight;
+            var weightFixed = val * weight;
             $(this).closest('tr').find('.weightOneProduct').text(weightFixed.toFixed(2));
 
             // обновляем кол-во заказа в корзине.
             var input = $(this).closest('.form-count').find('.form-count__value');
             var input_val = input.val();
             var product_id = input.data('id');
-            var url = window.location.origin+'/ajax/updateBasket.php';
-            data = {quantity: input_val, id: product_id};
+            var url = window.location.origin + '/ajax/updateBasket.php';
+            data = { quantity: input_val, id: product_id };
             $.ajax({
                 url: url,
                 data: data,
-                success: function (response) {
+                success: function success(response) {
                     console.log(JSON.parse(response));
                 },
-                error: function (error) {
-                    console.log(error);
+                error: function error(_error) {
+                    console.log(_error);
                 }
             });
             setItogoPrice();
@@ -74,14 +74,14 @@
 
         function setItogoPrice() {
             var sumItog = [];
-            $('.sumOneItem').each((i, e) => {
+            $('.sumOneItem').each(function (i, e) {
                 sumItog.push(e.innerHTML);
             });
             var sumStatic = 0;
-            for(let i=0;i<sumItog.length;i++){
+            for (var i = 0; i < sumItog.length; i++) {
                 sumStatic += Number(sumItog[i]);
             }
-            var sumItogo =  Number(sumStatic.toFixed(2));
+            var sumItogo = Number(sumStatic.toFixed(2));
             $('.basket-form-price>span:nth-child(1)').text(sumItogo);
             // console.log($($('.basket-form-price>span:nth-child(1)').eq(0)[0]).text());
         }
@@ -90,29 +90,29 @@
         $('.basket-table__del').click(function () {
             var id = $(this).data('id');
             var quantity = 0;
-            var url = window.location.origin+'/ajax/deleteItemFromBasket.php';
-            data = {quantity: quantity, id: id};
+            var url = window.location.origin + '/ajax/deleteItemFromBasket.php';
+            data = { quantity: quantity, id: id };
             $.ajax({
                 url: url,
                 data: data,
-                success: function (response) {
+                success: function success(response) {
                     var resp = JSON.parse(response);
-                    var lastRemove = $(`tr[data-id]`);
-                    var trremove = $(`[data-id=${resp.id}]`);
+                    var lastRemove = $('tr[data-id]');
+                    var trremove = $('[data-id=' + resp.id + ']');
                     console.log(lastRemove.length);
-                    if(lastRemove.length === 1) {
+                    if (lastRemove.length === 1) {
                         $('.basket-page').remove();
                         $('.appendIfBasketEmpty').append('<p>"К сожалению, ваша корзина пуста !"</p>');
                     }
                     trremove.remove();
 
-                    var urltocount = window.location.origin+'/ajax/countProductsInBasket.php';
+                    var urltocount = window.location.origin + '/ajax/countProductsInBasket.php';
                     $.ajax({
                         url: urltocount,
                         data: data,
-                        success: function (response) {
+                        success: function success(response) {
                             var resp = JSON.parse(response);
-                            if($('.header__basket-value_no_red').hasClass('header__basket-value_no_red')) {
+                            if ($('.header__basket-value_no_red').hasClass('header__basket-value_no_red')) {
                                 $('.header__basket-value_no_red').wrap("<div class='wraper_bck'></div>");
                                 $('.header__basket-value_no_red').removeClass('header__basket-value_no_red').addClass('header__basket-value');
                                 $('.header__basket-value').text(resp.length);
@@ -120,24 +120,22 @@
                             } else {
                                 $('.header__basket-value').text(resp.length);
                             }
-
                         },
-                        error: function (error) {
-                            console.log(error);
+                        error: function error(_error2) {
+                            console.log(_error2);
                         }
                     });
                 },
-                error: function (error) {
-                    console.log(error);
+                error: function error(_error3) {
+                    console.log(_error3);
                 }
             });
             return false;
         });
 
-
         // добавляем элемент в инфоблок при клике на кнопку оформить заказ.
         $('.basket-form__submit').click(function () {
-            var url = window.location.origin+'/ajax/addElement2Infoblock.php';
+            var url = window.location.origin + '/ajax/addElement2Infoblock.php';
             var unp = $('input[name="unp"]').val();
             var company_name = $('input[name="org_name"]').val();
             var contact_phone = $('input[name="phone"]').val();
@@ -154,7 +152,6 @@
             var fiz_phone = $('input[name="fiz_phone"]').val();
             var mail = $('input[name="mail"]').val();
             var fiz_adress = $('input[name="fiz-adress"]').val();
-
 
             data = {
                 unp: unp,
@@ -177,14 +174,14 @@
             $.ajax({
                 url: url,
                 data: data,
-                success: function (response) {
-                    var url2 = window.location.origin+'/ajax/addOrder2System.php';
+                success: function success(response) {
+                    var url2 = window.location.origin + '/ajax/addOrder2System.php';
                     var person_type_id = $("input:checked").data('person-type-id');
                     var userId = $("input:checked").data('user-id');
                     var sum = $($('.basket-form-price>span:nth-child(1)').eq(0)[0]).text();
                     // todo: добаить price
                     var data2 = {
-                        personTypeId:person_type_id,
+                        personTypeId: person_type_id,
                         userId: userId,
                         sum: sum,
                         unp: unp,
@@ -205,45 +202,44 @@
                     $.ajax({
                         url: url2,
                         data: data2,
-                        success: function (response) {
+                        success: function success(response) {
                             console.log(response);
-                            var url3 = window.location.origin+'/ajax/deleteAllFromBasket.php';
+                            var url3 = window.location.origin + '/ajax/deleteAllFromBasket.php';
                             $.ajax({
                                 url: url3,
-                                success: function (response) {
+                                success: function success(response) {
                                     console.log(response);
                                 },
-                                error: function (e) {
+                                error: function error(e) {
                                     console.log(e);
                                 }
                             });
                         },
-                        error: function (e) {
+                        error: function error(e) {
                             console.log(e);
                         }
                     });
 
                     orderSubmit();
                 },
-                error: function (error) {
-                    console.log(error);
+                error: function error(_error4) {
+                    console.log(_error4);
                 }
             });
             //return false;
         });
 
-
         // делаем автозаполнение из базы всех полей для юр. лиц.
         var dataFromBack = {};
         $('input[name="unp"]').on('keyup', function (e) {
-            var url = window.location.origin+'/ajax/findUnpInSystem.php';
+            var url = window.location.origin + '/ajax/findUnpInSystem.php';
             $.ajax({
                 url: url,
-                data: {data: e.target.value},
-                success: function (response) {
+                data: { data: e.target.value },
+                success: function success(response) {
                     var resp = JSON.parse(response);
                     dataFromBack = resp;
-                    if(resp !== null) {
+                    if (resp !== null) {
                         $('.add_all_info').css('display', 'block');
                         var company_name = dataFromBack.COMPANY_NAME,
                             contact_phone = dataFromBack.CONTACT_PHONE,
@@ -276,13 +272,11 @@
                         $('input[name="add_info"]').val('');
                     }
                 },
-                error: function (error) {
-                    console.log(error);
+                error: function error(_error5) {
+                    console.log(_error5);
                 }
             });
             return false;
         });
-
-
     });
-})(window);
+})(window, jQuery);
